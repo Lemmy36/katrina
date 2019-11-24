f7d30fef0ee505573c5543930607d15578337070
Florian Lippisch <florian@lippisch.eu>
2019-11-24 10:21:27 +0100

    fixed

--- header.php
+++ header.php
@@ -19,15 +19,11 @@
     <link rel="stylesheet" type="text/css" media="screen" href="main.css">
     <!-- Magnific Popup core CSS file -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="node_modules/magnific-popup/dist/magnific-popup.css">
 
     <link href="https://fonts.googleapis.com/css?family=Quicksand:300&display=swap" rel="stylesheet">
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="node_modules/magnific-popup/dist/jquery.magnific-popup.js"></script>
 
-
-    <script src="main.js"></script>
 
 </head>
 
--- index.php
+++ index.php
@@ -4,3 +4,21 @@
 include 'header.php'; 
 ?>
 
+
+<svg id="hearts" viewBox="-600 -400 1200 800" preserveAspectRatio="xMidYMid slice">
+  <defs>
+    <symbol  id="heart" viewBox="-69 -16 138 138">
+    <path d="M0,12
+             C 50,-30 110,50  0,120
+             C-110,50 -50,-30 0,12z"/>
+    </symbol>
+</defs>
+</svg>
+
+
+
+<script src="main.js"></script>
+
+
+</body>
+</html>
