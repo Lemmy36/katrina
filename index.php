2cd832f4c755bda2f08b26d858ed3fb149865e7b
Daniela <lemmy5738@gmail.com>
2019-11-26 22:09:31 +0100

    erg

--- index.php
+++ index.php
@@ -3,19 +3,22 @@
 <?php 
 include 'header.php'; 
 ?>
+<link rel="stylesheet" href="main.css">
+<script src="main.js"></script>
+<body>
+  <div id="countdown">
 
 
-<svg id="hearts" viewBox="-600 -400 1200 800" preserveAspectRatio="xMidYMid slice">
-  <defs>
-    <symbol  id="heart" viewBox="-69 -16 138 138">
-    <path d="M0,12
-             C 50,-30 110,50  0,120
-             C-110,50 -50,-30 0,12z"/>
-    </symbol>
-</defs>
-</svg>
-
-
+<div class="container">
+  <h1 id="head">nur die zahlen fehlen irgendwie</h1>
+  <ul>
+    <li><span id="days"></span>Tage</li>
+    <li><span id="hours"></span>Stunden</li>
+    <li><span id="minutes"></span>Minuten</li>
+    <li><span id="seconds"></span>Sekunden</li>
+  </ul>
+</div>
+</div>
 
 <script src="main.js"></script>
 
--- main.js
+++ main.js
@@ -34,3 +34,31 @@ function Frame(){
 }
 
 Frame()
+
+
+
+
+
+function countDown (){
+  
+  second = 1000,
+  minute = second * 60,
+  hour = minute * 60,
+  day = hour * 24;
+
+      document.getElementById('countdown')
+      .innerHTML = day + 'Tage' + hour + 'Stunden' + minute + 'Minuten'
+      + second + 'Sekunden';     
+     var now = new Date().getTime(),
+          distance = countDown - now;
+
+      document.getElementById('days').innerText = Math.floor(distance / (day)),
+        document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
+     document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute)),
+  document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
+      
+    
+
+  
+    document.addEventListener('DOMContentLoaded', countdown);
+}
